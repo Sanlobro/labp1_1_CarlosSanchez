cbdce0fbbf524c0aad3752b5f0a54ad0f00a3f27
Sanlobro <143653000+Sanlobro@users.noreply.github.com>
2025-04-25 15:05:38 -0600

    Commit 2 y final

--- src/labp1_1_carlossanchez/Labp1_1_CarlosSanchez.java
+++ src/labp1_1_carlossanchez/Labp1_1_CarlosSanchez.java
@@ -4,9 +4,10 @@
  */
 package labp1_1_carlossanchez;
 
+import java.util.Scanner;
 /**
  *
- * @author G5530
+ * @author Carlos Sanchez 
  */
 public class Labp1_1_CarlosSanchez {
 
@@ -14,7 +15,45 @@ public class Labp1_1_CarlosSanchez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int respuesta = -1;
+        Scanner read = new Scanner(System.in);
+        System.out.println("sds");
+        while(respuesta != 0){
+        
+            System.out.println("Menu principal");
+            System.out.println("Opcion 1. Mensaje");
+            System.out.println("Opcion 2. Calculadora");
+            System.out.println("Opcion 3. Edades");
+            
+            respuesta = read.nextInt();
+            
+            if (respuesta == 1){
+                System.out.println("olsa soy yo");
+                
+            } else if (respuesta == 2){
+                System.out.println("Ingrese el numero uno");
+                int num1 = read.nextInt();
+                System.out.println("Ingrese el segundo numero");
+                int num2 = read.nextInt();
+                
+                int resultado = num1 * num2;
+                
+                System.out.println("Resultado" +resultado);
+                
+            } else if (respuesta == 3){
+                System.out.println("Ingrese su edad");
+                int edad = read.nextInt();
+                
+                if (edad >= 18){
+                    System.out.println("Usted es mayor de Edad");
+                } else {
+                    System.out.println("Usted no es mayor de edad xd");
+                }
+                    
+            }    
+       }
+        
     }
     
 }
